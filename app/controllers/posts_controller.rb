7b3af4e018c44a3428d74d064fcbe4c53eb7f99a
dex <dexter.blair@gmail.com>
2017-01-14 00:59:06 -0500

    Stub in of posts controller.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,26 @@
 class PostsController < ApplicationController
+	def index
+	end
+
+	def new
+	end
+
+	def create
+	end
+
+	def edit
+	end
+
+	def show
+	end
+
+	def destroy
+	end
+
+	def post_params
+	end
+
+	def find_post
+	end
+
 end
